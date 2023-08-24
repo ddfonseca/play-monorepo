2014216172a91203d038e480fb01b2823c4f4501
David Fonseca <olfdavid@gmail.com>
2023-08-24 19:45:48 -0300

    add app02 with environments

--- apps/app1/src/app/app.component.ts
+++ apps/app1/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { environment } from '@channels/shared/util-environment';
+
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],

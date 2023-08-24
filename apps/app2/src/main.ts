5311f75daf28ec104d4a73113b533ba271de3216
David Fonseca <olfdavid@gmail.com>
2023-08-24 20:01:20 -0300

    add env for apps

--- apps/app2/src/main.ts
+++ apps/app2/src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { environment } from '@channels/shared/util-environment';
+
+console.log(environment.production);
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
